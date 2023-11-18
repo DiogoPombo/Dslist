a739f926d5acdd2a5a5ddf7f9f79b8b281e120b1
DiogoPombo <digo_pombo@hotmail.com>
2023-11-18 19:43:09 -0300

    Query in code

--- src/main/java/com/pombo/java/dslistpombo/dto/GameMinDTO.java
+++ src/main/java/com/pombo/java/dslistpombo/dto/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.pombo.java.dslistpombo.dto;
 
 import com.pombo.java.dslistpombo.entities.Game;
+import com.pombo.java.dslistpombo.projections.GameMinProjection;
 
 public class GameMinDTO {
 	
@@ -20,6 +21,14 @@ public class GameMinDTO {
 		year = entity.getYear();
 		imgUrl = entity.getImgUrl();
 		shortDescription = entity.getShortDescription();
+	}
+	
+	public GameMinDTO(GameMinProjection projection) {
+		id = projection.getId();
+		title = projection.getTitle();
+		year = projection.getYear();
+		imgUrl = projection.getImgUrl();
+		shortDescription = projection.getShortDescription();
 	}
 
 	public Long getId() {
--- src/main/java/com/pombo/java/dslistpombo/services/GameService.java
+++ src/main/java/com/pombo/java/dslistpombo/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.pombo.java.dslistpombo.dto.GameDTO;
 import com.pombo.java.dslistpombo.dto.GameMinDTO;
 import com.pombo.java.dslistpombo.entities.Game;
+import com.pombo.java.dslistpombo.projections.GameMinProjection;
 import com.pombo.java.dslistpombo.repositories.GameRepository;
 
 @Service
@@ -27,6 +28,13 @@ public class GameService {
 	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 			List<Game> result = gameRepository.findAll();
+			return result.stream().map(x -> new GameMinDTO(x)).toList();
+		
+		}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId) {
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
 			return result.stream().map(x -> new GameMinDTO(x)).toList();
 		
 		}
