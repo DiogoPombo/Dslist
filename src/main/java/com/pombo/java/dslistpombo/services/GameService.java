cf31d34d403eff6d3e8cd36b0ff85f77b6a6245a
DiogoPombo <digo_pombo@hotmail.com>
2023-11-18 17:33:53 -0300

    Get game by id

--- src/main/java/com/pombo/java/dslistpombo/services/GameService.java
+++ src/main/java/com/pombo/java/dslistpombo/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.pombo.java.dslistpombo.dto.GameDTO;
 import com.pombo.java.dslistpombo.dto.GameMinDTO;
 import com.pombo.java.dslistpombo.entities.Game;
 import com.pombo.java.dslistpombo.repositories.GameRepository;
@@ -15,6 +17,14 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		return new GameDTO(result);
+		
+	}
+	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 			List<Game> result = gameRepository.findAll();
 			return result.stream().map(x -> new GameMinDTO(x)).toList();
