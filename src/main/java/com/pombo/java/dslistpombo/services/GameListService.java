825a49796ed56e877a4d055715cc9b74739285c3
DiogoPombo <digo_pombo@hotmail.com>
2024-02-02 21:51:23 -0300

    Move games on list

--- src/main/java/com/pombo/java/dslistpombo/services/GameListService.java
+++ src/main/java/com/pombo/java/dslistpombo/services/GameListService.java
@@ -8,7 +8,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.pombo.java.dslistpombo.dto.GameListDTO;
 import com.pombo.java.dslistpombo.entities.GameList;
+import com.pombo.java.dslistpombo.projections.GameMinProjection;
 import com.pombo.java.dslistpombo.repositories.GameListRepository;
+import com.pombo.java.dslistpombo.repositories.GameRepository;
 
 @Service
 public class GameListService {
@@ -16,13 +18,30 @@ public class GameListService {
 	@Autowired
 	private GameListRepository gameListRepository;
 	
+	@Autowired
+	private GameRepository gameRepository;
+	
 	@Transactional(readOnly = true)
 	public List<GameListDTO> findAll() {
 			List<GameList> result = gameListRepository.findAll();
 			return result.stream().map(x -> new GameListDTO(x)).toList();
 		
 		}
+	@Transactional	
+	public void move(Long listId, int sourceIndex, int destinationIndex) {
+		List<GameMinProjection> list = gameRepository.searchByList(listId);
 		
+		GameMinProjection obj = list.remove(sourceIndex);
+		list.add(destinationIndex, obj);
+		
+		int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+		int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+		
+		for (int i = min; i <= max; i++) {
+			gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+		}
+	}
+	
 	}
 	
 
